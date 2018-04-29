7e20a9a03d6142988e77d2dcb411e513e829b211
Aman Deep Singh <aman150084@gmail.com>
2018-04-30 01:35:50 +0530

    smaple b_k is done

--- sampler/main.py
+++ sampler/main.py
@@ -2,9 +2,35 @@ import numpy as np
 
 #x_t: n x d
 # z: t x k
-# b: k x d
+# B: k x d
 
-def sample_b_k(x_t, y_t, z, t, k, beta, sigma_beta):
+def sample_b_k(x_t, y_t, z, t, k, beta, sigma_beta, B):
+    #B_k is B without the kth row
+    print("task:", t)
+    print("k:", k)
+    B_k = np.delete(B, (t), axis=0) 
     x_hat = z[t, k] * x_t
-    y_hat = y_t - x_t, 
+    sub = np.matmul(x_t,  np.transpose(B_k))
+    z_temp = np.delete(z[t], (k), axis=0)
+    # print(np.transpose(z_temp))
+    # print(sub)
+    sub = np.matmul(sub, np.transpose(z_temp))
+    y_hat = y_t - sub
+    variance = np.linalg.inv(beta * np.matmul(np.transpose(x_hat), x_hat) + sigma_beta)
+    mean = np.matmul(variance, beta * np.matmul(np.transpose(x_hat), y_hat))
+    return mean, variance
 
+
+n = 3
+d = 2
+t = 4
+k = 5
+x_t = np.random.random((n,d))
+y_t = np.random.random((n,1))
+z = np.random.random((t,k))
+B = np.random.random((k,d))
+# print(x_t)
+# print(z)
+# print(B)
+sigma_beta = np.random.random((d,d))
+sample_b_k(x_t, y_t, z, 2, 2, 1, sigma_beta, B)
