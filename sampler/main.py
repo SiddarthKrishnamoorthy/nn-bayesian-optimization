ca5b00288fcbfa7c958f0a50d7768f04664816a8
Aman Deep Singh <aman150084@gmail.com>
2018-04-30 03:38:18 +0530

    iterations written

--- sampler/main.py
+++ sampler/main.py
@@ -28,12 +28,12 @@ def sample_b_k(x_t, y_t, z, t, k, beta, sigma_beta, B):
     # print("sigma",variance)
     return np.random.multivariate_normal(mean, variance)
 
-def sample_z_t(x_t, y_t, z, t, k, beta, sigma_z, B):
+def sample_z_t(x_t, y_t, z, beta, sigma_z, B):
     x_hat = np.matmul(x_t, np.transpose(B))
-    print(x_hat)
+    # print(x_hat)
     variance = np.linalg.inv(beta* np.matmul(np.transpose(x_hat), x_hat) + sigma_z)
     mean = np.transpose(np.matmul(variance, beta * np.matmul(np.transpose(x_hat), y_t)))[0]
-    print("mean", mean)
+    # print("mean", mean)
     return np.random.multivariate_normal(mean, variance)
     # print("reached here")
 
@@ -42,8 +42,11 @@ n = 3
 d = 2
 t = 4
 k = 5
-x_t = np.random.random((1,d))
-y_t = np.random.random((1,1))
+x = []
+y = []
+for t1 in range(t):
+    x.append(np.random.random((1,d)))
+    y.append(np.random.random((1,1)))
 z = np.random.random((t,k))
 B = np.random.random((k,d))
 # print(x_t)
@@ -51,7 +54,15 @@ B = np.random.random((k,d))
 # print(B)
 sigma_beta = np.random.random((d,d))
 sigma_z = np.random.random((k,k))
-print("b samples", sample_b_k(x_t, y_t, z, 2, 2, 1, sigma_beta, B))
-print("z samples", sample_z_t(x_t, y_t, z, 2, 2, 1, sigma_z, B))
+# print("b samples", sample_b_k(x_t, y_t, z, 2, 2, 1, sigma_beta, B))
+# print("z samples", sample_z_t(x_t, y_t, z, 2, 2, 1, sigma_z, B))
+for iteration in range(100):
+    for t1 in range(t):
+        x_t = x[t1]
+        y_t = y[t1]
+        for k1 in range(k):
+            B[k1] = sample_b_k(x_t, y_t, z, t1, k1, 1, sigma_beta, B)
+            # print(iteration)
+        z[t1] =  sample_z_t(x_t, y_t, z, 1, sigma_z, B)
 
-
+# B[k1] = 
